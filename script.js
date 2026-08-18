5c25e35193acaa14088b58e7de0cb13cab30b20e
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2026-08-18 15:24:50 +0000

    Added projects, removed photos

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Adds nav bar to all pages with nav element
 if (document.querySelector('nav')) {
-    document.querySelector('nav').innerHTML = `<a href="index.html">Home</a><a href="projects.html">Projects</a><a href="photos.html">Photos</a>`
+    document.querySelector('nav').innerHTML = `<a href="index.html">Home</a><a href="projects.html">Projects</a>`
 }
+// <a href="photos.html">Photos</a>
